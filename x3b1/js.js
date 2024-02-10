c41b196c23fbcf7880a948ec97af76d86b5bd532
aleksandersciubisz <aleksander.sciubisz@gmail.com>
2024-02-10 12:28:44 +0100

    Add files via upload
    Standard backup -> website almost ready, with all the placeholders for the real app

--- x3b1/js.js
+++ x3b1/js.js
@@ -24,13 +24,39 @@ hax.forEach((el) => {
 	})
 })
 
+const smallHax = document.querySelectorAll('.small-hax')
+
+smallHax.forEach((el) => {
+	el.addEventListener('mouseover', (event) => {
+		const { target } = event
+		let iteration = 0
+		const chars = ['x', '3', 'b', '1']
+		target.dataset.originalText = target.innerText // Zapisuje oryginalny tekst
+		clearInterval(target.interval)
+		target.interval = setInterval(() => {
+			if (iteration < chars.length * 1) {
+				const index = iteration % chars.length
+				target.innerText = chars[index] // Wyświetla kolejne znaki
+				iteration++
+			} else {
+				clearInterval(target.interval)
+				target.innerText = target.dataset.originalText // Przywraca oryginalny tekst
+			}
+		}, 60) // Przerwa między zmianą znaku
+	})
+})
 // -----------------------
 const nav = document.getElementById('nav')
-const menu = document.getElementById('mobile-menu')
+const menu = document.getElementById('menu')
 
-nav.addEventListener('click', navToggle)
+nav.addEventListener('click', toggleMenu)
 
-function navToggle() {
+function toggleMenu() {
 	nav.classList.toggle('open')
 	menu.classList.toggle('show-menu')
+	nav.textContent = nav.classList.contains('open') ? 'exit' : 'more'
+	nav.setAttribute(
+		'data-value',
+		nav.classList.contains('open') ? 'exit' : 'more'
+	)
 }
